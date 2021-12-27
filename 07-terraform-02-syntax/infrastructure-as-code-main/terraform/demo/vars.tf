5a5e03f167dc30a4d20722224a00dc19f68e1215
Alexander <anantahari@yandex.ru>
2021-12-27 14:33:29 +0300

    07-02

---
+++ 07-terraform-02-syntax/infrastructure-as-code-main/terraform/demo/vars.tf
@@ -0,0 +1,25 @@
+variable "yc_token" {
+   default = "AQAAAAA241WeAATuwfdGrzE74krGtR1QsxCCc0c"
+ }
+variable "yc_cloud_id" {
+  default = "b1gku58k1k8tvqul8qbq"
+}
+variable "yc_folder_id" {
+  default ="b1g82tojtne9luuk16s5"
+}
+variable "yc_region" {
+  default = "ru-central1-a"
+}
+variable "yc_project_name" {
+  default = "netology"
+}
+variable "yc_network_id" {
+  default = "enp09opd7ksaedcorclb"
+}
+variable "yc_subnet_id" {
+  default = "e9bjnga63shpd7873vq0"
+}
+variable "yc_zone" {
+  default = "ru-central1-a"
+}
+
--- terraform/main.tf
+++ terraform/main.tf
@@ -8,8 +8,8 @@ terraform {
 }
 
 provider "yandex" {
-  token     = "AQAAAAA241WeAATuwfdGrzE74krGtR1QsxCCc0c"
-  cloud_id  = "b1gku58k1k8tvqul8qbq"
-  folder_id = "b1g82tojtne9luuk16s5"
+  token     = ""
+  cloud_id  = ""
+  folder_id = ""
   zone      = "ru-central1-a"
 }
